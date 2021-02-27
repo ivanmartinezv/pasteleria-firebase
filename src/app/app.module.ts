1df03c07947747e06d1052a317adc03fd574e2fa
ivanmartinezv <ivmartez@gmail.com>
2021-02-27 05:44:20 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 //modulos
-import { AppRoutingModule } from "./app-routing.module";
+//import { AppRoutingModule } from "./app-routing.module";
 //componentes
 import { ProductoComponent } from "./producto/producto.component";
 //firebase
@@ -14,7 +14,7 @@ import { environment } from "../environments/environment";
   declarations: [AppComponent, ProductoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule, //rutas
+    //AppRoutingModule, //rutas
     AngularFireModule.initializeApp(environment.firebase), //configuracion de firebase
     FormsModule, //formularios
     ReactiveFormsModule //formularios
